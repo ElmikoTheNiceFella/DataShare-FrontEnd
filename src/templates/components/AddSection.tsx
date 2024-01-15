582e000bef68b108c4fcb48a12dce9ed80c26c40
ElmikoTheNiceFella <akprogamer23@gmail.com>
2024-01-15 12:21:36 +0300

    Organizing Modals

---
+++ src/templates/components/AddSection.tsx
@@ -0,0 +1,12 @@
+import styles from '../../styles/modules/modal.module.scss'
+import { useMemo } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faXmark } from '@fortawesome/free-solid-svg-icons'
+
+const AddSection = () => {
+  return (
+    <div>AddSection</div>
+  )
+}
+
+export default AddSection
--- src/templates/components/Modal.tsx
+++ src/templates/components/Modal.tsx
@@ -1,17 +1,20 @@
 import styles from '../../styles/modules/modal.module.scss'
-import {useState, useEffect, useMemo} from 'react'
+import {useMemo} from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faXmark } from '@fortawesome/free-solid-svg-icons'
+import { AddSection } from '.'
 
 type ModalProps = {
   open: boolean;
   close: () => void;
   type: string;
-  // changeContent: (type:string, content:{[key:string]:any}) => void;
+  handleChanges: (type:string, content:{[key:string]:any}) => void;
 }
 
 const handleType = (type:string) => {
   switch(type) {
     case 'addSection':
-      return {name: 'Edit Section', properties: {
+      return {name: 'Add Section', properties: {
         placeholder: 'Section Name',
         id: 'add-section'
       }}
@@ -20,9 +23,25 @@ const handleType = (type:string) => {
   }
 }
 
-const Modal = ({open, close, type}:ModalProps) => {
+const Modal = ({open, close, type, handleChanges}:ModalProps) => {
 
   const values = useMemo(() => handleType(type), [type])
+
+  const handleSubmit = (e:React.SyntheticEvent) => {
+    e.preventDefault();
+    const form = e.target as HTMLFormElement;
+    const data = new FormData(form);
+    let result:{[key:string]:any} = {};
+    switch(type) {
+      case 'addSection':
+        result.newSection = data;
+        break;
+      default:
+        break;
+    }
+    console.log(data.entries())
+    console.log(result)
+  }
 
   // const handleChange = () => changeContent(type, )
 
@@ -35,15 +54,22 @@ const Modal = ({open, close, type}:ModalProps) => {
       }} 
       onClick={close}
       className={styles.modalContainer}>
-      <div onClick={e => e.stopPropagation()} className={styles.modal}>
-        {/* Name */}
-        <h2>{values.name}</h2>
-        {/* Properties */}
-        <div className={styles.inputContainer}>
-          <input type='text' className={styles.modalTextInput} id={values.properties?.id} />
-          <label className={styles.modalInputPlaceholder} htmlFor={values.properties?.id}>{values.properties?.placeholder}</label>
-        </div>
-        <button className={styles.submit}>Save Changes</button>
+      <div style={{
+        opacity: open ? 1 : 0
+      }} onClick={e => e.stopPropagation()} className={styles.modal}>
+        <form onSubmit={handleSubmit}>
+          <button onClick={close} className={styles.close}>
+            <FontAwesomeIcon icon={faXmark} />
+          </button>
+          {/* Name */}
+          <h2>{values.name}</h2>
+          {/* Properties */}
+          <div className={styles.inputContainer}>
+            <input type='text' name={values.properties?.id} className={styles.modalTextInput} id={values.properties?.id} />
+            <label className={styles.modalInputPlaceholder} htmlFor={values.properties?.id}>{values.properties?.placeholder}</label>
+          </div>
+          <button type='submit' className={styles.submit}>Save Changes</button>
+        </form>
       </div>
     </div>
   )
