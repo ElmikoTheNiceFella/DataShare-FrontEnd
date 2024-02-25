f55b63b5d0ac116d0ccc1015d1cf082f1ac797fe
ElmikoTheNiceFella <akhadkhou@gmail.com>
2024-02-25 08:05:31 +0300

    First GET request test

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -3,4 +3,8 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+app.get('/', (req, res) => {
+  res.send("Hello World")
+})
+
 app.listen(process.env.PORT || 3000, () => console.log('Server is running...'))
